aaa765dbc8315bbabfd7d1adab0cb2be129d8d31
Joao Messi <joao.messi@movemais.com>
2019-03-28 21:29:37 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/tradutorsdmkt/Constantes.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.sdm.tradutorsdmkt
+
+object Constantes {
+    // Constantes usadas para acesso ao WS de tradução
+    val URL_BASE = "https://od-api.oxforddictionaries.com/api/v1/"
+    val END_POINT = "entries"
+    val APP_ID_FIELD = "app_id"
+    val APP_ID_VALUE = "0be341b9" // Preencher com seu app_id
+    val APP_KEY_FIELD = "app_key"
+    val APP_KEY_VALUE = "378f40573ba9b6d7cb51424dbaa9e999" // Preeencher com seu app_key
+}
